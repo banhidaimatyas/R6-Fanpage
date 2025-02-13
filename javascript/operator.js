f0d21e849bc8d53195af059b8193a906c4dd1f02
nemethzalan07 <nemeth.zalan@students.jedlik.eu>
2025-02-13 11:54:18 +0100

    operátorok oldal továbbfejlesztése, javascript elkezdése

---
+++ javascript/operator.js
@@ -0,0 +1,40 @@
+const operators = [
+    {
+        name: "Rook:",
+        description: "",
+    },
+    {
+        name: "Aruni".
+        description: "",
+    },
+    {
+        name: "IQ",
+        description: "",
+    },
+    {
+        name: "Maestro"
+        description: "",
+    },
+    {
+        description: "Tachanka",
+    },
+    {
+        description: "Thundebird",
+        image: "url(../)"
+    },
+    {
+        description: "Vigil",
+    },
+    {
+        description: "Wamai",
+    },
+    {
+        description: "Zero",
+    },
+];
+
+let active = operators[0];
+
+function Img(){
+    document.getElementById("operator-description").inner
+}
