c1cf36adb0932fe2cd41f3b2874b017f62e070e5
nemethzalan07 <nemeth.zalan@students.jedlik.eu>
2025-02-20 11:11:41 +0100

    operátor menü befejezése

--- javascript/operator.js
+++ javascript/operator.js
@@ -1,37 +1,83 @@
 const operators = [
     {
+        name: "Aruni:",
+        image: "../Images/operator_images/aruni_1.png",
+        health: 3,
+        speed: 1,
+    },
+
+    
+    {
         name: "Rook:",
-        description: "",
+        image: "../Images/operator_images/rook_1.png",
+        health: 3,
+        speed: 1,
     },
+
     {
-        name: "Aruni",
-        description: "",
+        name: "Vigil:",
+        image: "../Images/operator_images/vigil_1.png",
+        health: 1,
+        speed: 3,
     },
+    
     {
-        name: "IQ",
-        description: "",
+        name: "IQ:",
+        image: "../Images/operator_images/ip_1.png",
+        health: 1,
+        speed: 3,
     },
+
     {
-        name: "Maestro",
-        description: "",
+        name: "Tachanka:",
+        image: "../Images/operator_images/tachanka_1.png",
+        health: 3,
+        speed: 1,
     },
+
     {
-        description: "Tachanka",
+        name: "Wamai:",
+        image: "../Images/operator_images/wamai_1.png",
+        health: 2,
+        speed: 2,
     },
+
+
     {
-        description: "Thundebird",
+        name: "Maestro:",
+        image: "../Images/operator_images/maestro_1.png",
+        health: 3,
+        speed: 1,
+    },
+
+    {
+        name: "Thunderbird:",
         image: "../Images/operator_images/thunderbird_1.png",
+        health: 2,
+        speed: 2,
     },
+
+
     {
-        description: "Vigil",
-    },
-    {
-        description: "Wamai",
-    },
-    {
-        description: "Zero",
+        name: "Zero:",
+        image: "../Images/operator_images/zero_1.png",
+        health: 1,
+        speed: 2,
     },
 ];
 
 let active = operators[0];
+render(operators[0]);
+
+function SelectedItem(index){
+    active = operators[index];
+    render();
+}
+
+function render(){
+    document.getElementById("operator-img").setAttribute("src", active.image);
+    document.getElementById("operator-name").innerText = active.name;
+}
+
+
 
