d8ac8333bdc548cb69b0070335ed920c6be3f68e
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2025-02-22 19:13:29 +0100

    képek, adatszerkezet, script

---
+++ javascript/guesser.js
@@ -0,0 +1,31 @@
+const Oregon = {
+  levels: {
+    0: {
+      rooms: {
+        1: { coordinates: "", roomName: "" },
+        2: { coordinates: "", roomName: "" },
+        3: { coordinates: "", roomName: "" },
+        4: { coordinates: "", roomName: "" },
+        5: { coordinates: "", roomName: "" },
+      },
+    },
+    1: {
+      rooms: {
+        1: { coordinates: "", roomName: "" },
+        2: { coordinates: "", roomName: "" },
+        3: { coordinates: "", roomName: "" },
+        4: { coordinates: "", roomName: "" },
+        5: { coordinates: "", roomName: "" },
+      },
+    },
+    2: {
+      rooms: {
+        1: { coordinates: "", roomName: "" },
+        2: { coordinates: "", roomName: "" },
+        3: { coordinates: "", roomName: "" },
+        4: { coordinates: "", roomName: "" },
+        5: { coordinates: "", roomName: "" },
+      },
+    },
+  },
+};
