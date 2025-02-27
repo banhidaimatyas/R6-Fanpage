e09d59d8c73144160bbc2ea044f0e71c726bb79c
nemethzalan07 <nemeth.zalan@students.jedlik.eu>
2025-02-27 16:43:09 +0100

    reszponzivítás megoldása

--- javascript/operator.js
+++ javascript/operator.js
@@ -1,7 +1,7 @@
 const operators = [
     {
         name: "Aruni:",
-        image: "../Images/operator_images/aruni_1.png",
+        image: '../Images/operator_images/aruni_1.png',
         health: 3,
         speed: 1,
     },
@@ -9,35 +9,35 @@ const operators = [
     
     {
         name: "Rook:",
-        image: "../Images/operator_images/rook_1.png",
+        image: '../Images/operator_images/rook_1.png',
         health: 3,
         speed: 1,
     },
 
     {
         name: "Vigil:",
-        image: "../Images/operator_images/vigil_1.png",
+        image: '../Images/operator_images/vigil_1.png',
         health: 1,
         speed: 3,
     },
     
     {
         name: "IQ:",
-        image: "../Images/operator_images/ip_1.png",
+        image: '../Images/operator_images/ip_1.png',
         health: 1,
         speed: 3,
     },
 
     {
         name: "Tachanka:",
-        image: "../Images/operator_images/tachanka_1.png",
+        image: '../Images/operator_images/tachanka_1.png',
         health: 3,
         speed: 1,
     },
 
     {
         name: "Wamai:",
-        image: "../Images/operator_images/wamai_1.png",
+        image: '../Images/operator_images/wamai_1.png',
         health: 2,
         speed: 2,
     },
@@ -45,14 +45,14 @@ const operators = [
 
     {
         name: "Maestro:",
-        image: "../Images/operator_images/maestro_1.png",
+        image: '../Images/operator_images/maestro_1.png',
         health: 3,
         speed: 1,
     },
 
     {
         name: "Thunderbird:",
-        image: "../Images/operator_images/thunderbird_1.png",
+        image: '../Images/operator_images/thunderbird_1.png',
         health: 2,
         speed: 2,
     },
@@ -60,7 +60,7 @@ const operators = [
 
     {
         name: "Zero:",
-        image: "../Images/operator_images/zero_1.png",
+        image: '../Images/operator_images/zero_1.png',
         health: 1,
         speed: 2,
     },
@@ -75,7 +75,7 @@ function SelectedItem(index){
 }
 
 function render(){
-    document.getElementById("operator-img").setAttribute("src", active.image);
+    document.body.style.backgroundImage = `url(${active.image})`;
     document.getElementById("operator-name").innerText = active.name;
 }
 
